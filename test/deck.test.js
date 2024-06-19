822b23581a0a4f568a3eb0023069912ed60508b1
Max Topan <maxtopan@hotmail.co.uk>
2024-06-19 21:50:29 +0100

    add deck shuffle tests

--- test/deck.test.js
+++ test/deck.test.js
@@ -6,11 +6,29 @@ describe('Deck class', () => {
     beforeEach(() => {
         deck = new Deck();
     });
-    
+
     /// placeholder test ///
     test('should mock shuffle method', () => {
         deck.shuffle = jest.fn();
         deck.shuffle();
         expect(deck.shuffle).toHaveBeenCalled();
-      });
+    });
+
+    test('should instantiate deck once, then keep same deck', () => {
+        let a = deck.cards.toString();
+        let b = deck.cards.toString();
+
+        expect(a).toEqual(b);
+    });
+
+    describe('shuffle', () => {
+        test('shuffle() should... shuffle cards', () => {
+            let a = [...deck.cards];
+            deck.shuffle();
+            let b = [...deck.cards];
+
+            expect(a).not.toEqual(b);
+            expect(a.sort()).toEqual(b.sort());
+        });
+    });
 })
