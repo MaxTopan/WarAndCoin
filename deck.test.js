b9b993c82c4057dd8b040b86cccb23531d8c5c33
Max Topan <maxtopan@hotmail.co.uk>
2024-06-19 15:51:36 +0100

    fix babel translation, refactor folders

--- deck.test.js
+++
@@ -1,5 +0,0 @@
-const deck = require('./deck.js')
-
-test('makes a deck obj', () => {
-    expect(deck.Deck).toBe(Deck);
-});
---
+++ test/deck.test.js
@@ -0,0 +1,17 @@
+//const deck = require('../src/deck.js')
+import Deck from '../src/deck.js';
+
+describe('Deck class', () => {
+    let deck;
+
+    beforeEach(() => {
+        deck = new Deck();
+    });
+    
+    /// placeholder test ///
+    test('should mock shuffle method', () => {
+        deck.shuffle = jest.fn();
+        deck.shuffle();
+        expect(deck.shuffle).toHaveBeenCalled();
+      });
+})
